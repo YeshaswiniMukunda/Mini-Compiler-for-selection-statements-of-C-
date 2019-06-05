ea508cb29bd6424ba1607f440d9a45059fa33da0
YeshaswiniMukunda <43567860+YeshaswiniMukunda@users.noreply.github.com>
2019-06-05 13:46:55 +0530

    Intermediate Code Generation

---
+++ Intermediate Code Generation/input.cpp
@@ -0,0 +1,83 @@
+
+// checks if the number is positive
+
+/*jhddcjcjhskcjh
+dchwdjch
+jdchhc*/
+/*#if(k+8) then k=18;else c=s;
+#http://www.professionalcipher.com/2017/07/intermediate-code-generation-using-lex-and-yacc-using-switch-case-and-control-flow.html
+#line 83 checking for else condition to generate goto statement.*/
+/*
+#implement arrays
+# ast
+#any two optimization
+#scope*/
+
+
+
+
+#include<"new.h">
+#include<"yup.h">
+
+class boss {
+private:
+	int acasdf;
+public:
+	int lsafd;
+	void fn1() {
+		int adsdff;
+		cout << "fn1 myClass " << endl;
+	}
+	int ggfff;
+	void f()
+	{
+		int hsdjhdad;
+		cout<<"H"<<endl;
+	}
+	int hdggddjjhd;
+};
+
+
+void main(){
+int a0,b;
+float c;
+double d;
+l=a0+b;
+boss o1,o2;
+if(a==0)
+{
+ a=9;
+}
+else if(s<=7)
+{
+	s=0;
+}
+else if(p>=     9)
+{
+ d=8;
+}
+else {
+	switch(a)
+{
+	case 8:if(a==0)
+{
+ a=9;
+}
+	break;
+	case 7 :e=9;
+}
+g=89;
+}
+
+switch(a)
+{
+	case 8:if(a==0)
+{
+ a=9;
+}
+	break;
+	case 7 :e=9;
+	default:e=987;
+}
+cout<<"happy"<<endl;
+cin>>a;}
