46bd0dc62367392d063b865e70da8ca082f88b54
VarunaKS <31765337+VarunaKS@users.noreply.github.com>
2019-06-05 14:47:46 +0530

    Optimization

---
+++ Optimization/input.cpp
@@ -0,0 +1,87 @@
+
+// checks if the number is positive
+
+/*jhddcjcjhskcjh
+dchwdjch
+jdchhc*/
+#include<"new.h">
+#include<"conio.h">
+//0
+//scopes=[0,1,2,3,4,5,6]
+//scopes_count=7
+//recentscopecount=2
+//recentscope=[0,4,5]
+//sco=5
+//m=5
+class boss { 
+private:
+//1
+	int acasdf;
+public:
+	int lsafd;
+	void fn1() {
+		//2
+		cout << "fn1 myClass " << endl;
+	}
+	//1
+	void f()
+	{
+	//3
+		cout<<"H"<<endl;
+	}
+	//1
+};
+
+//0
+void main(){
+//5
+int a0,b;
+float c;
+ int k;
+ int h;
+double d;
+int j;
+if(a==0)
+{
+	//6
+ a=9;
+ h=1;
+ k=a*h;
+}
+//5
+else if(s<=7)
+{
+	s=2;
+	b=1;
+	c=2;
+	p=c/s;
+}
+else if(p>=     9)
+{
+ ad=8;
+}
+else {
+	j=4;
+	switch(a)
+{
+	case 8:if(a==0)
+{
+ a=9;
+ s=j+a;
+}
+	break;
+	case 7 :e=9;
+}
+}
+
+switch(a)
+{
+	case 8:if(a==0)
+{
+ a=9;
+}
+	break;
+	case 7 :e=9;
+}
+cout<<"happy"<<endl;
+cin>>a;}
